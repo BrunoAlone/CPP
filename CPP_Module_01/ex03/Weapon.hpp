ebdc2f4fa6225509a68e3231c7827c1659cf7130
Bruno <brolivei@student.42porto.com>
2023-12-11 16:52:14 +0000

    Starting ex03

---
+++ CPP_Module_01/ex03/Weapon.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brolivei <brolivei@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 16:45:15 by brolivei          #+#    #+#             */
+/*   Updated: 2023/12/11 16:51:43 by brolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+
+#include <iostream>
+
+class	Weapon
+{
+	private:
+		std::string	type;
+	public:
+		const std::string	&getType();
+		void	setType(std::string newType);
+};
+
+#endif
