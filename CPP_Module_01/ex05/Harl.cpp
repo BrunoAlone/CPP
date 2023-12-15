129dc74515f266f96306e2974661dc985f4fd1eb
BrunoAlone <brolivei@student.42porto.com>
2023-12-15 12:37:46 +0000

    Starting ex05

---
+++ CPP_Module_01/ex05/Harl.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bruno <bruno@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 12:09:42 by bruno             #+#    #+#             */
+/*   Updated: 2023/12/15 12:36:40 by bruno            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+void	Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my\
+	 7XL-double-cheese-triple-pickle-special-ketchup burger.\
+	  I really do!\n";
+}
+
+void	Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put\
+enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for\
+years whereas you started working here since last month.\n";
+}
+
+void	Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void	Harl::complain(std::string level)
+{
+	
+}
--- CPP_Module_01/ex05/Harl.hpp
+++ CPP_Module_01/ex05/Harl.hpp
@@ -1,29 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bruno <bruno@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/12 14:16:50 by brolivei          #+#    #+#             */
-/*   Updated: 2023/12/15 11:50:56 by bruno            ###   ########.fr       */
+/*   Created: 2023/12/15 12:01:26 by bruno             #+#    #+#             */
+/*   Updated: 2023/12/15 12:21:25 by bruno            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include "Replace.hpp"
-
-int	main(int argc, char **argv)
-{
-	if (argc != 4)
-	{
-		std::cout << "Invalid arguments.\n\n";
-		std::cout << "Way to use:\n\n";
-		std::cout << "	./name_of_the_programa <filename> <s1> <s2>\n";
-		return (0);
-	}
-
-	Replace	newFile(argv[1], argv[2], argv[3]);
-	newFile.replaceFile();
-	return (0);
-}
+Harl also has a public member function that calls the four member functions above
+depending on the level passed as parameter:
+void complain( std::string level );
