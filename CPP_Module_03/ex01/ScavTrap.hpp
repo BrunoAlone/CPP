0d03ebd59ae473002f6edf2edd97dad322427b61
Bruno Joel Oliveira Soares <brolivei@c1r9s2.42porto.com>
2024-01-17 10:15:52 +0000

    Module03 ex00 DOne

--- CPP_Module_03/ex01/ScavTrap.hpp
+++ CPP_Module_03/ex01/ScavTrap.hpp
@@ -1,26 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: brolivei < brolivei@student.42porto.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/16 14:46:40 by brolivei          #+#    #+#             */
-/*   Updated: 2024/01/16 15:00:24 by brolivei         ###   ########.fr       */
+/*   Created: 2024/01/16 16:45:41 by brolivei          #+#    #+#             */
+/*   Updated: 2024/01/16 16:45:52 by brolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
 
-int	main()
-{
-	std::string	name = "Manuel";
-	ClapTrap	A(name);
-	A.attack("Jose");
-	A.takeDamage(4);
-	A.takeDamage(6);
-	A.takeDamage(1);
-	A.beRepaired(2);
-
-	return (0);
-}
