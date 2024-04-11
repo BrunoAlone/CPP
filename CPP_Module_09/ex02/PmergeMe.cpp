b42da4166322b4ba53b0af083ae9c4b8696f248d
Bruno Joel Oliveira Soares <brolivei@c2r2s1.42porto.com>
2024-04-11 16:03:23 +0100

    Module09 ex02: Change list container to deque. Need to perform parsing

--- CPP_Module_09/ex02/PmergeMe.cpp
+++ CPP_Module_09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: brolivei <brolivei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 16:40:48 by brolivei          #+#    #+#             */
-/*   Updated: 2024/04/11 11:26:35 by brolivei         ###   ########.fr       */
+/*   Updated: 2024/04/11 16:02:27 by brolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@ PmergeMe::PmergeMe()
 PmergeMe::~PmergeMe()
 {}
 
-void	PmergeMe::mergeSort(char **argv, int size)
+int	PmergeMe::mergeSort(char **argv, int size)
 {
-	justPrintBefore(argv, size);
+	justPrintBefore(argv, size); // This function will perfom the parse too
 
 	mergeSortVecStart(argv, size);
+	mergeSortDequeStart(argv, size);
+	return (0);
 }
 
 // ===============VECTOR===============
@@ -39,7 +41,7 @@ void	PmergeMe::mergeSortVecStart(char **argv, int size)
 
 	mergeSortVec(vec, 0, vec.size() - 1);
 
-	std::cout << std::setw(9) << std::left << "After: ";
+	std::cout << std::setw(9) << std::left << "After(Vec): ";
 	for (it = vec.begin(); it != vec.end(); it++)
 		std::cout << " " << *it;
 	std::cout << std::endl;
@@ -63,8 +65,8 @@ void	PmergeMe::mergeSortVec(std::vector<int>& vec, int const begin, int const en
 
 void	PmergeMe::mergeVec(std::vector<int>& vec, int const left, int const mid, int const right)
 {
-	int	const subVecOne = mid - left + 1;
-	int	const subVecTwo = right - mid;
+	int	const	subVecOne = mid - left + 1;
+	int	const	subVecTwo = right - mid;
 
 	std::vector<int>	leftVec(subVecOne);
 	std::vector<int>	rightVec(subVecTwo);
@@ -109,16 +111,87 @@ void	PmergeMe::mergeVec(std::vector<int>& vec, int const left, int const mid, in
 }
 
 
+// ===============Deque===============
 
+void	PmergeMe::mergeSortDequeStart(char **argv, int size)
+{
+	this->start = clock();
+	std::deque<int>				deque;
+	std::deque<int>::iterator	it;
 
+	for (int i = 1; i < size; i++)
+		deque.push_back(atoi(argv[i]));
 
+	mergeSortDeque(deque, 0, deque.size() - 1);
 
+	std::cout << std::setw(9) << std::left << "After(deque): ";
+	for (it = deque.begin(); it != deque.end(); it++)
+		std::cout << " " << *it;
+	std::cout << std::endl;
+	this->end = clock();
+	this->timeSpendList = ((double(this->end - this->start) / CLOCKS_PER_SEC) * 1000000) - this->timeSpendVec;
+	std::cout << "Time to process a range of " << deque.size();
+	std::cout << " elements with std::deque<int> : " << this->timeSpendList;
+	std::cout << " microseconds" << std::endl;
+}
 
+void	PmergeMe::mergeSortDeque(std::deque<int>& list, int const begin, int const end)
+{
+	if (begin >= end)
+		return ;
 
+	int	mid = begin + (end - begin) / 2;
+	mergeSortDeque(list, begin, mid);
+	mergeSortDeque(list, mid + 1, end);
+	mergeDeque(list, begin, mid, end);
+}
 
+void	PmergeMe::mergeDeque(std::deque<int>& deque, int const left, int const mid, int const right)
+{
+	int	const	subDeqOne = mid - left + 1;
+	int	const	subDeqTwo = right - mid;
 
+	std::deque<int>	leftDeq(subDeqOne);
+	std::deque<int>	rightDeq(subDeqTwo);
 
+	for (int i = 0; i < subDeqOne; i++)
+		leftDeq[i] = deque[left + i];
+	for (int j = 0; j < subDeqTwo; j++)
+		rightDeq[j] = deque[mid + 1 + j];
 
+	int	indexOfsubDeqOne = 0;
+	int	indexOfsubDeqTwo = 0;
+	int	indexOfMergeDeq = left;
+
+	while (indexOfsubDeqOne < subDeqOne && indexOfsubDeqTwo < subDeqTwo)
+	{
+		if (leftDeq[indexOfsubDeqOne] <= rightDeq[indexOfsubDeqTwo])
+		{
+			deque[indexOfMergeDeq] = leftDeq[indexOfsubDeqOne];
+			indexOfsubDeqOne++;
+		}
+		else
+		{
+			deque[indexOfMergeDeq] = rightDeq[indexOfsubDeqTwo];
+			indexOfsubDeqTwo++;
+		}
+		indexOfMergeDeq++;
+	}
+
+	while (indexOfsubDeqOne < subDeqOne)
+	{
+		deque[indexOfMergeDeq] = leftDeq[indexOfsubDeqOne];
+		indexOfsubDeqOne++;
+		indexOfMergeDeq++;
+	}
+
+	while (indexOfsubDeqTwo < subDeqTwo)
+	{
+		deque[indexOfMergeDeq] = rightDeq[indexOfsubDeqTwo];
+		indexOfsubDeqTwo++;
+		indexOfMergeDeq++;
+	}
+}
 
 
 void	PmergeMe::justPrintBefore(char **argv, int size)
--- CPP_Module_09/ex02/PmergeMe.hpp
+++ CPP_Module_09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: brolivei <brolivei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 16:36:33 by brolivei          #+#    #+#             */
-/*   Updated: 2024/04/11 11:20:19 by brolivei         ###   ########.fr       */
+/*   Updated: 2024/04/11 15:54:35 by brolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@
 #include <cstdlib>
 #include <iomanip>
 #include <ctime>
+#include <iterator>
 
 #include <vector>
-#include <list>
+#include <deque>
 
 class	PmergeMe
 {
@@ -30,17 +31,23 @@ class	PmergeMe
 		clock_t		end;
 
 		double		timeSpendVec;
+		double		timeSpendList;
 
 		void	mergeSortVecStart(char **argv, int size);
 		void	mergeSortVec(std::vector<int>& vec, int const begin, int const end);
 		void	mergeVec(std::vector<int>& vec, int const left, int const mid, int const right);
 
+		void	mergeSortDequeStart(char **argv, int size);
+		void	mergeSortDeque(std::deque<int>& list, int const begin, int const end);
+		void	mergeDeque(std::deque<int>& list, int const left, int const mid, int const right);
+
 		void	justPrintBefore(char **argv, int size);
+		bool	mergeParse(char **argv, int size);
 	public:
 		PmergeMe();
 		~PmergeMe();
 
-		void	mergeSort(char **argv, int size);
+		int	mergeSort(char **argv, int size);
 };
 
 #endif
